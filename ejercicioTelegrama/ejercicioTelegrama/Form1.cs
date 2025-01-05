186f76e1bb176617520c35c7281c0c0fe75fb26a
MykytaVavulin2 <yasha.tusovka@gmail.com>
2025-01-06 00:26:52 +0100

    Cambié los precios

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -42,11 +42,11 @@ namespace ejercicioTelegrama
             {
                 if (numPalabras <= 10)
                 {
-                    coste = 2.5;
+                    coste = 3;
                 }
                 else
                 {
-                    coste = 0.5 * numPalabras;
+                    coste = 6;
                 }
             }
             else
